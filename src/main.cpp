3b317268f662e2b452b088ea7aea976eaaec9fd3
aSchuhknecht <adam.schuhknecht@gmail.com>
2018-11-02 22:12:13 -0400

    formula changes Nov 2nd

--- src/main.cpp
+++ src/main.cpp
@@ -7,144 +7,129 @@ InterruptIn rear(D5);
 PwmOut sparkCut(D6);
 
 #define ledpin LED3
-#define Pi  3.14//15926535897
+#define Pi  3.1415
 
 const double diameter = 1.3333333; // in feet
-const double radius = 0.875; // in feet
+const double radius = 0.666666; // in feet
 const double circ = diameter*Pi;
 
+double rightSpeed = 0;
+double leftSpeed = 0;
+double rearSpeed = 0;
+double frontSpeed = 0;
+unsigned short delta = 0;
+unsigned char highByte = 0;
+unsigned char lowByte = 0;
+
 DigitalOut out(ledpin);
+
+CAN can1(D10, D2); // CAN bus pins
 
 Timer tf1;
 Timer tf2;
 Timer r;
 
-long time1 = 0;
-long time2 = 0;
-long time3 = 0;
+long time1 = 1;
+long time2 = 1;
+long time3 = 1;
 
 int rightCount = 0;
 int leftCount = 0;
 int rearCount = 0;
 
-bool flag2 = false;
+double speed;
 
-
-void calculateSpeed(double ticks, int micros);
- 
-
+double calculateSpeed(double ticks, int micros);
+  
 void right_triggered()
 {
-    rightCount++;
-    out.write(!out.read());
 
     if (rightCount == 0) {
-
-        tf1.start();       
+        tf1.start();
+        rightCount++;
     }
 
-    if (rightCount == 5) {
+    else if (rightCount == 5) {
 
         tf1.stop();
-        time1 = r.read_us();
-
-        printf("%s","Time1 is ");
-        printf("%f\n", time1);
-        calculateSpeed(5.0, time1);
+                
+        time1 = tf1.read_us();
+        rightSpeed = calculateSpeed(5.0, time1);
 
         rightCount = 0;
         tf1.reset();
     }
+    else  {
+        rightCount++;    
+    }
 }
+
 
 void left_triggered()
 {
 
-    out.write(!out.read());
-
     if (leftCount == 0) {
-        
-        printf("%s","Count is: ");
-        printf("%d\n",leftCount);
-        
         tf2.start();
-        printf("%s\n","Started");
         leftCount++;
-        flag2 = true;
     }
 
-    if ((leftCount > 0) && (leftCount < 5) && !flag2) {
-        
-        printf("%s","Count is: ");
-        printf("%d\n",leftCount);
-
-        leftCount++;
-        
-    }
-
-    if (leftCount == 5) {
+    else if (leftCount == 5) {
 
         tf2.stop();
-
-        printf("%s","Count is: ");
-        printf("%d\n",leftCount);
-        
                         
         time2 = tf2.read_us();
-
-        printf("%s","Time2 is ");
-        printf("%ld\n",time2);
-        calculateSpeed(5.0, time2);
+        leftSpeed = calculateSpeed(5.0, time2);
 
         leftCount = 0;
         tf2.reset();
-        
     }
-    flag2 = false;
+    else  {
+        leftCount++;    
+    }
 }
+
 
 void rear_triggered()
 {
-    rearCount++;
-    out.write(!out.read());
 
     if (rearCount == 0) {
-
-        r.start();       
+        r.start();
+        rearCount++;
     }
-    if (rearCount == 20) {
+
+    else if (rearCount == 25) {
 
         r.stop();
+                        
         time3 = r.read_us();
-
-        printf("%s","Time3 is ");
-        printf("%f\n", time3);
-        calculateSpeed(20.0, time3);
+        rearSpeed = calculateSpeed(25.0, time3);
 
         rearCount = 0;
         r.reset();
     }
-
+    else  {
+        rearCount++;    
+    }
 }
 
-void calculateSpeed(double ticks, int micros) {
+double calculateSpeed(double ticks, int micros) {
 
+    double revs;
     double speed; 
     double seconds = micros/1000000.0;
 
-    
     double ticksPerSecond = (ticks/seconds); // ticks per second 
 
-    double revs = ticksPerSecond/5.0; //revs per second
+    if (ticks == 5) {
+        revs = ticksPerSecond/25.0; //revs per second
+    }
+    else {
+        revs =  ticksPerSecond/100.0;
+    }
 
-    double angular = revs * 2.0 * Pi;  // angular speed
-
-    speed = angular*radius; // feet per second
-
-    double mph = speed/(1.467);
-   
-    printf("%s","mph is ");
-    printf("%f\n",mph);
+    speed = revs*2.8553; // 2.8553 is the number that converts rev/s to linear speed 
+                        // (wheel size dependent)
+    return speed;
     
 }
 
@@ -161,5 +146,48 @@ int main()
     left.rise(&left_triggered);
     rear.rise(&rear_triggered);
 
-    
+    while (1) {
+       
+
+        // wait(1);
+        // printf("%f\n",rightSpeed);
+        // printf("%f\n",leftSpeed);
+        // printf("%f\n\n",rearSpeed);
+
+        if (leftSpeed >= rightSpeed) {
+            frontSpeed = leftSpeed;
+        }
+        else {
+            frontSpeed = rightSpeed;
+        }
+
+        delta = (frontSpeed / rearSpeed) *100;
+        printf("%f\n",delta);
+        highByte = (delta >> 8) & 0xFF;
+        lowByte = delta & 0xFF;
+
+
+        can1.frequency(250000);
+        can1.mode(CAN::Normal);
+
+        wait(1);
+        CANMessage msg;
+        // if (can1.read(msg)){
+        //     printf("\n%d", msg.id);
+        //     printf("%s", " ");
+        //     for (int i = 0; i < msg.len; i++){
+        //         printf("%d ", msg.data[i]);
+        //     }
+        //      printf("\n");
+        // }
+        msg.id = 1;
+        msg.data[0] = round(rightSpeed);
+        msg.data[1] = round(leftSpeed);
+        msg.data[2] = round(rearSpeed);
+        msg.data[3] = round (lowByte);
+        msg.data[4] = round (highByte);
+
+        can1.write(msg);
+
+    }   
 }
