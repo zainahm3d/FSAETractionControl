fa413c322cec53fdbf31a6b2d3f9f7779c319b0e
aSchuhknecht <adam.schuhknecht@gmail.com>
2018-12-31 14:07:59 -0500

    removed bunch of stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,14 @@
 #include <mbed.h>
-#include "PID.h"
 
-#include "nRF24L01.h"
-#include "RF24.h"
-#include <SPI.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-// InterruptIn left(D11);
-// InterruptIn right(D12);
-// InterruptIn rear(D12);
+#define ledpin LED3
+#define Pi 3.1415
 
 DigitalOut ig(D6);
-
- #define ledpin LED3
- #define Pi 3.1415
+DigitalOut out(ledpin);
 
 const double diameter = 1.3333333; // in feet
 const double radius = 0.666666;    // in feet
@@ -24,31 +17,12 @@ const double circ = diameter * Pi;
 double rightSpeed = 0;
 double leftSpeed = 0;
 double rearSpeed = 0;
-
 double frontSpeed = 0;
 
-// unsigned short delta = 0;
-// unsigned char highByte = 0;
-// unsigned char lowByte = 0;
-
-DigitalOut out(ledpin);
-
-// #define nrf_CE A1
-// #define nrf_CSN A0
-// #define spi_SCK A4
-// #define spi_MOSI A6
-// #define spi_MISO A5
-
-// RF24 radio(spi_MOSI, spi_MISO, spi_SCK, nrf_CE, nrf_CSN);
-
-const uint64_t pipe = 0xE8E8F0F0E1LL;
-
-//CAN can1(D10, D2); // CAN bus pins
 
 Timer tf1;
 Timer tf2;
 Timer r;
-//Timer accelTimer;
 
 long time1 = 1;
 long time2 = 1;
@@ -58,13 +32,12 @@ int rightCount = 0;
 int leftCount = 0;
 int rearCount = 0;
 
-double speed;
 
 double calculateSpeed(double ticks, int micros);
 
+
 void right_triggered()
 {
-
     out = !out;
 
     float test = tf1.read();
@@ -81,7 +54,6 @@ void right_triggered()
 
     else if (rightCount == 5)
     {
-
         tf1.stop();
 
         time1 = tf1.read_us();
@@ -98,7 +70,6 @@ void right_triggered()
 
 void left_triggered()
 {
-
     out = !out;
 
     float test = tf2.read();
@@ -132,7 +103,6 @@ void left_triggered()
 
 void rear_triggered()
 {
-
     out = !out;
 
     float test = r.read();
@@ -189,45 +159,16 @@ double calculateSpeed(double ticks, int micros)
     return speed;
 }
 
-/*
-double calculateAcceleration(double prev, double newSpeed, double t) {
-
-    double t2 = t/1000000.0;
-
-    double accel = (newSpeed - prev)/(t2);
-
-    return accel; 
-}
-*/
 
 int main()
 {
 
-    // radio.begin();
-    // radio.openWritingPipe(pipe);
-    // radio.setPALevel(RF24_PA_HIGH);
-    // radio.setDataRate(RF24_250KBPS);
-    // radio.stopListening();
-
-    //right.fall(&right_triggered);
-    //left.fall(&left_triggered);
-    //rear.fall(&rear_triggered);
-
- 
-    //left.mode(PullUp);
-    //right.mode(PullUp);
-    //rear.mode(PullUp);
- 
     ig = 0;
 
     while (1)
     {
 
-        wait(0.2);
-        
-        //out = !out;
-        // printf("%s","Rear speed: ");
-        // printf("%f\n",rearSpeed);
+
 
         if (leftSpeed >= rightSpeed)
         {
@@ -237,18 +178,10 @@ int main()
         {
             frontSpeed = rightSpeed;
         }
-        // frontSpeed = 1.5;
-        // rearSpeed = 1.2;
+ 
 
         // printf("%s","Rear speed: ");
         // printf("%f\n",rearSpeed);
-
-        // int f = round(frontSpeed);
-        // int r = round(rearSpeed);
-        // int n = '\n';
-
-        //radio.write(&frontSpeed, sizeof(frontSpeed));
-        //radio.write(&rearSpeed, sizeof(rearSpeed));
         
             float diff = abs(rearSpeed - frontSpeed);
 
@@ -264,8 +197,6 @@ int main()
                 ig = 0;
             }
 
-            // wait(.1);
-            // printf("%f\n", diff);
         
     }
 }
